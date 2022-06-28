3e8030c09ec1e1228d806e9fba85e4e04542930b
Bruno Eberhardt <83976168+Harteup@users.noreply.github.com>
2022-06-28 20:51:31 -0300

    Add files via upload

---
+++ ListaDupla/src/num1000/App.java
@@ -0,0 +1,49 @@
+package num1000;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ListaDupla listaC = new ListaDupla();
+        ListaDupla listaD = new ListaDupla();
+        int [] vetor = new int[1000];
+        
+        for(int i = 0; i<100; i++){
+            int randomNumber=(int)((Math.random() * 9999) - 4545);
+            vetor[i] = randomNumber;
+        }
+        
+        for(int i = 0; i<100; i++){
+            System.out.println(vetor[i]);
+        }
+
+
+        int n = 100;
+        int min = 0;
+        
+        for(int i = 0; i < (n-1); i++){
+        
+            min = i;
+            
+            for(int j = (i+1); j < n; j++){
+                if(vetor[j] < vetor[min]){
+                    min = j;
+                }
+            }
+            
+            if(vetor[i] != vetor[min]){
+                int aux = vetor[i];
+                vetor[i] = vetor[min];
+                vetor[min] = aux;
+            }
+        }
+
+        for(int i = 0; i<100; i++){
+            listaD.AdicionarNoComeco(vetor[i]);
+        }
+
+        System.out.println("\nDecrescente: " +  listaD.Imprimir()); 
+        for(int i = 99; i>=0; i--){
+            listaC.AdicionarNoComeco(vetor[i]);
+        }
+        System.out.println("Crescente: " +  listaC.Imprimir());
+    }
+}
